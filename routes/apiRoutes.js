be267e9f3f62554aad547262c126fb6e87cf9f59
Allison Barnard <allisonbarnard07@gmail.com>
2020-09-20 14:22:25 -0400

    Updated

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,7 +2,6 @@ var db = require("../models");
 
 module.exports = function(app) {
 
-    // Used by api.js to get last workout
     app.get("/api/workouts", (req, res) => {
         db.Workout.find({})
         .then(workout => {
@@ -12,8 +11,7 @@ module.exports = function(app) {
             res.json(err);
         });
     });
-    
-    // Creates a new workout in the workout database
+
     app.post("/api/workouts", async (req, res)=> {
         try{
             const response = await db.Workout.create({type: "workout"})
@@ -24,16 +22,12 @@ module.exports = function(app) {
         }
     })
 
-    // Used by api.js to add an exercise to a workout
     app.put("/api/workouts/:id", ({body, params}, res) => {
-        // console.log(body, params)
         const workoutId = params.id;
         let savedExercises = [];
 
-        // gets all the currently saved exercises in the current workout
         db.Workout.find({_id: workoutId})
             .then(dbWorkout => {
-                // console.log(dbWorkout)
                 savedExercises = dbWorkout[0].exercises;
                 res.json(dbWorkout[0].exercises);
                 let allExercises = [...savedExercises, body]
